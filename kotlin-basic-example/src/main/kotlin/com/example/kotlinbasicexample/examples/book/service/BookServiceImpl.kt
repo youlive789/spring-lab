1898537706325205b1613186870fff3d452db93e
gwkim <youlive789@gmail.com>
2022-07-20 08:19:19 +0900

    feat: kotlin example restructuring

--- kotlin-basic-example/src/main/kotlin/com/example/kotlinbasicexample/examples/book/service/BookServiceImpl.kt
+++ kotlin-basic-example/src/main/kotlin/com/example/kotlinbasicexample/examples/book/service/BookServiceImpl.kt
@@ -7,10 +7,10 @@ import org.springframework.stereotype.Service
 @Service
 class BookServiceImpl(val bookRepository: BookRepository) : BookService {
     override fun getBooks(): List<BookDto> {
-        return bookRepository.findAll().map {book -> book.toDto()};
+        return bookRepository.findAll().map { book -> book.toDto() };
     }
 
     override fun getBookById(id: Long): BookDto? {
-        return bookRepository.findById(id).map { book -> book.toDto() }.orElseGet {BookDto()};
+        return bookRepository.findById(id).map { book -> book.toDto() }.orElseGet { BookDto() };
     }
 }
--- kotlin-basic-example/src/test/kotlin/com/example/kotlinbasicexample/examples/book/repository/BookRepositoryTests.kt
+++ kotlin-basic-example/src/test/kotlin/com/example/kotlinbasicexample/examples/book/repository/BookRepositoryTests.kt
@@ -1,4 +1,15 @@
 package com.example.kotlinbasicexample.examples.book.repository
 
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
+@DisplayName("BookRepository 테스트")
 class BookRepositoryTests {
+
+    @DisplayName("조회 시 정상적으로 책 데이터를 가져와야 함")
+    @Test
+    fun getBooksTest() {
+
+    }
+
 }
